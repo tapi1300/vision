a4172975fc284977c257b8d186a74b08d4af1bf3
tapi1300 <pedop4@gmail.com>
2021-12-07 12:08:35 +0100

    Examen

---
+++ pEXAMEN/p6/build/CMakeFiles/pcl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl.dir/main.cpp.o"
+  "pcl"
+  "pcl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pEXAMEN/p6/build/CMakeFiles/view3d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/view3d.dir/view3D.cpp.o"
+  "view3d"
+  "view3d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/view3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
